c4bd2924cb56c19185b5f40fa4bdb812be88a2a5
Josh Duggan <josh.duggan20@gmail.com>
2020-06-09 20:18:39 +0100

    Completed TODOs (all caps because Tom said so)

--- src/main/java/com/westosia/godpowers/ClickListener.java
+++ src/main/java/com/westosia/godpowers/ClickListener.java
@@ -20,121 +20,92 @@ public class ClickListener implements Listener {
 
     private final Main instance = Main.getInstance();
 
-    // TODO: Move duplicate code into methods
-    // TODO: Remove useless if statements
-    // TODO: Remove Explicity typed arguments in "new ArrayList"
-    // TODO: #addAll can be converted to a paramaterised constructor on line above
-
     @EventHandler
     public void onClick(PlayerInteractEvent e) {
         Player player = e.getPlayer();
+        if (e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getType().equals(Material.DIAMOND_SWORD)) {
+            int modelData = e.getItem().getItemMeta().getCustomModelData();
+            if ((e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) && modelData == 1) {
+                ArrayList<Entity> chaosVictims = new ArrayList<>(player.getNearbyEntities(10, 10, 10));
 
-        if (e.getAction().equals(Action.RIGHT_CLICK_AIR) && e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getType().equals(Material.DIAMOND_SWORD) && e.getItem().getItemMeta().getCustomModelData() == 1) {
-            ArrayList<Entity> airVictims = new ArrayList<Entity>();
-            airVictims.addAll(player.getNearbyEntities(10, 10, 10));
-
-            for (Entity victim : airVictims) {
-                if (victim instanceof Entity && victim != player) { // TODO: Replace instanceof Entity with LivingEntity
-                    WestosiaAPI.getParticleEmitter().playParticle(player.getLocation(), Particle.DRAGON_BREATH);
-                    victim.setVelocity(new Vector(0, 0.6, 0));
-                    victim.setVelocity(new Vector(player.getLocation().getDirection().multiply(10).getX(), 2, player.getLocation().getDirection().multiply(10).getZ()));
-                    victim.setFireTicks(100);
-                    victim.getWorld().playSound(victim.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
+                for (Entity victim : chaosVictims) {
+                    if (victim instanceof LivingEntity && victim != player) {
+                        WestosiaAPI.getParticleEmitter().playParticle(player.getLocation(), Particle.DRAGON_BREATH);
+                        victim.setVelocity(new Vector(player.getLocation().getDirection().multiply(10).getX(), 2, player.getLocation().getDirection().multiply(10).getZ()));
+                        victim.setFireTicks(100);
+                        victim.getWorld().playSound(victim.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
+                    }
                 }
-            }
-            Random rand = new Random();
-            int upperbound = 20;
-            int randomX = rand.nextInt(upperbound);
-            int randomY = 150;
-            int randomZ = rand.nextInt(upperbound);
-            Location newLoc = player.getLocation().add(randomX, randomY, randomZ);
-            randomY = newLoc.getWorld().getHighestBlockYAt(newLoc);
-            newLoc.setY(randomY + 1);
-            player.teleport(newLoc);
-            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
+                Random rand = new Random();
+                int upperbound = 20;
+                int randomX = rand.nextInt(upperbound);
+                int randomY = 150;
+                int randomZ = rand.nextInt(upperbound);
+                Location newLoc = player.getLocation().add(randomX, randomY, randomZ);
+                randomY = newLoc.getWorld().getHighestBlockYAt(newLoc);
+                newLoc.setY(randomY + 1);
+                player.teleport(newLoc);
+                player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
 
-        } else if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getType().equals(Material.DIAMOND_SWORD) && e.getItem().getItemMeta().getCustomModelData() == 1) {
-            ArrayList<Entity> airVictims = new ArrayList<Entity>();
-            airVictims.addAll(player.getNearbyEntities(10, 10, 10));
+            } else if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && modelData == 2) {
 
-            for (Entity victim : airVictims) {
-                if (victim instanceof Entity && victim != player) { // TODO: Replace instanceof Entity with LivingEntity
-                    WestosiaAPI.getParticleEmitter().playParticle(player.getLocation(), Particle.DRAGON_BREATH);
-                    victim.setVelocity(new Vector(0, 0.6, 0));
-                    victim.setVelocity(new Vector(player.getLocation().getDirection().multiply(10).getX(), 2, player.getLocation().getDirection().multiply(10).getZ()));
-                    victim.setFireTicks(100);
-                    victim.getWorld().playSound(victim.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
+                ArrayList<Entity> airVictims = new ArrayList<>(player.getNearbyEntities(10, 10, 10));
+                for (Entity victim : airVictims) {
+                    if ((victim instanceof LivingEntity || victim.getType().equals(EntityType.ARROW)) && victim != player) {
+                        Vector dif = victim.getLocation().toVector().subtract(player.getLocation().toVector());
+                        victim.setVelocity(dif.normalize().multiply(3).add(new Vector(0, 1, 0)));
+                    }
                 }
-            }
-            Random rand = new Random();
-            int upperbound = 20;
-            int randomX = rand.nextInt(upperbound);
-            int randomY = 150;
-            int randomZ = rand.nextInt(upperbound);
-            Location newLoc = player.getLocation().add(randomX, randomY, randomZ);
-            randomY = newLoc.getWorld().getHighestBlockYAt(newLoc);
-            newLoc.setY(randomY + 1);
-            player.teleport(newLoc);
-            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_EVOKER_CAST_SPELL, 1, 1);
-        } else if (e.getAction().equals(Action.RIGHT_CLICK_AIR) && e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getType().equals(Material.DIAMOND_SWORD) && e.getItem().getItemMeta().getCustomModelData() == 2) {
 
-            ArrayList<Entity> airVictims = new ArrayList<Entity>();
-            airVictims.addAll(player.getNearbyEntities(10, 10, 10));
-            for (Entity victim : airVictims) {
-                if (victim instanceof Entity && victim != player) { // TODO: Replace instanceof Entity with LivingEntity
-                    victim.setVelocity(new Vector(0, 0.6, 0));
-                    victim.setGravity(false);
-                    Bukkit.getScheduler().runTaskLater(instance, () -> {
+            } else if (e.getAction().equals(Action.RIGHT_CLICK_AIR) && modelData == 2) {
 
-                        victim.setGravity(true);
-                        victim.setVelocity(new Vector(0, -4, 0));
-                    }, 60);
+                ArrayList<Entity> airVictims = new ArrayList<>(player.getNearbyEntities(10, 10, 10));
+                for (Entity victim : airVictims) {
+                    if ((victim instanceof LivingEntity || victim.getType().equals(EntityType.ARROW)) && victim != player) {
+                        victim.setVelocity(new Vector(0, 0.6, 0));
+                        victim.setGravity(false);
+                        WestosiaAPI.getSoundEmitter().playSound(player.getLocation(), 10, Sound.ITEM_ELYTRA_FLYING, 1, 2);
+                        Bukkit.getScheduler().runTaskLater(instance, () -> {
+
+                            victim.setGravity(true);
+                            victim.setVelocity(new Vector(0, -4, 0));
+                            WestosiaAPI.getSoundEmitter().playSound(player.getLocation(), 10, Sound.ENTITY_PLAYER_ATTACK_SWEEP);
+                        }, 60);
+                    }
                 }
-            }
 
-        } else if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getType().equals(Material.DIAMOND_SWORD) && e.getItem().getItemMeta().getCustomModelData() == 2) {
-
-            ArrayList<Entity> airVictims = new ArrayList<Entity>();
-            airVictims.addAll(player.getNearbyEntities(10, 10, 10));
-            for (Entity victim : airVictims) {
-                if (victim instanceof Entity && victim != player) { // TODO: Replace instanceof Entity with LivingEntity
-                    Vector dif = victim.getLocation().toVector().subtract(player.getLocation().toVector());
-                    victim.setVelocity(dif.normalize().multiply(3).add(new Vector(0, 1, 0)));
-                }
-            }
-
-        } else if (e.getAction().equals(Action.RIGHT_CLICK_AIR) && e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getType().equals(Material.DIAMOND_SWORD) && e.getItem().getItemMeta().getCustomModelData() == 3) {
-            List<Block> los = player.getLineOfSight(null, 32);
-            World world = player.getWorld();
-            world.strikeLightning(player.getLocation());
-            player.setGlowing(true);
-            Bukkit.getScheduler().runTaskLater(instance, () -> {
+            } else if (e.getAction().equals(Action.RIGHT_CLICK_AIR) && modelData == 3) {
+                List<Block> los = player.getLineOfSight(null, 32);
+                World world = player.getWorld();
                 world.strikeLightning(player.getLocation());
+                player.setGlowing(true);
                 Bukkit.getScheduler().runTaskLater(instance, () -> {
                     world.strikeLightning(player.getLocation());
-                }, 8);
-            }, 4);
-            Bukkit.getScheduler().runTaskLater(instance, () -> {
-                player.setGlowing(false);
-                int i;
-                for (i = 0; i < los.size(); i++) {
-                    int finalI = i;
                     Bukkit.getScheduler().runTaskLater(instance, () -> {
-                        world.strikeLightning(los.get(finalI).getLocation());
+                        world.strikeLightning(player.getLocation());
+                    }, 8);
+                }, 4);
+                Bukkit.getScheduler().runTaskLater(instance, () -> {
+                    player.setGlowing(false);
+                    int i;
+                    for (i = 0; i < los.size(); i++) {
+                        int finalI = i;
+                        Bukkit.getScheduler().runTaskLater(instance, () -> {
+                            world.strikeLightning(los.get(finalI).getLocation());
 
 
-                    }, i);
-                }
-            }, 28);
+                        }, i);
+                    }
+                }, 28);
 
 
-        } else if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getType().equals(Material.DIAMOND_SWORD) && e.getItem().getItemMeta().getCustomModelData() == 3) {
-            ArrayList<Entity> lightningVictims = new ArrayList<Entity>();
-            lightningVictims.addAll(player.getNearbyEntities(10, 10, 10));
-            World world = player.getWorld();
-            for (Entity victim : lightningVictims) {
-                if (victim instanceof LivingEntity && victim != player) {
-                    world.strikeLightning(victim.getLocation());
+            } else if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && modelData == 3) {
+                ArrayList<Entity> lightningVictims = new ArrayList<>(player.getNearbyEntities(10, 10, 10));
+                World world = player.getWorld();
+                for (Entity victim : lightningVictims) {
+                    if (victim instanceof LivingEntity && victim != player) {
+                        world.strikeLightning(victim.getLocation());
+                    }
                 }
             }
         }
